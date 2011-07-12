e0a7337c2f55849c1437d1f1ec3786458456e1ac
Dave Johnson <dave@nitobi.com>
2011-07-11 17:16:44 -0700

    Restructure again since Im not going to get to where I want this to be today. Ship it

---
+++ app/tmp/ios/Classes/ExampleAppAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  ExampleAppAppDelegate.h
+//  ExampleApp
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+
+@interface ExampleAppAppDelegate : PhoneGapDelegate {
+}
+
+@end
+
