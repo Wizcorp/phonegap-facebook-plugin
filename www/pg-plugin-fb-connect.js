ae60b5aa1c47826aa551536878238eb6c5039470
Fil Maj <filip.maj@nitobi.com>
2012-01-11 16:01:06 -0800

    added failure callbacks to PhoneGap connect plugin, removed extraneous/duplicate files

--- www/pg-plugin-fb-connect.js
+++ www/pg-plugin-fb-connect.js
@@ -1,36 +1,37 @@
 PG = ( typeof PG == 'undefined' ? {} : PG );
 PG.FB = {
-    init: function(apiKey) {
-        // create the fb-root element if it doesn't exist
-        if (!document.getElementById('fb-root')) {
-            var elem = document.createElement('div');
-            elem.id = 'fb-root';
-            document.body.appendChild(elem);
-        }
-        PhoneGap.exec(function() {
-        	var session = JSON.parse(localStorage.getItem('pg_fb_session') || '{"expires":0}');
-        	if (session && session.expires > new Date().valueOf()) {
-        		FB.Auth.setSession(session, 'connected');
-            }
-        }, null, 'com.phonegap.facebook.Connect', 'init', [apiKey]);
-    },
-    login: function(a, b) {
-        b = b || { perms: '' };
-        PhoneGap.exec(function(e) { // login
-            localStorage.setItem('pg_fb_session', JSON.stringify(e.session));
-            FB.Auth.setSession(e.session, 'connected');
-            if (a) a(e);
-        }, null, 'com.phonegap.facebook.Connect', 'login', b.perms.split(',') );
-    },
-    logout: function(cb) {
-        PhoneGap.exec(function(e) {
-            FB.Auth.setSession(null, 'notConnected');
-            if (cb) cb(e);
-        }, null, 'com.phonegap.facebook.Connect', 'logout', []);
-    },
-    getLoginStatus: function(cb) {
-        PhoneGap.exec(function(e) {
-            if (cb) cb(e);
-        }, null, 'com.phonegap.facebook.Connect', 'getLoginStatus', []);
+  init: function(apiKey, fail) {
+    // create the fb-root element if it doesn't exist
+    if (!document.getElementById('fb-root')) {
+      var elem = document.createElement('div');
+      elem.id = 'fb-root';
+      document.body.appendChild(elem);
     }
+    PhoneGap.exec(function() {
+      var session = JSON.parse(localStorage.getItem('pg_fb_session') || '{"expires":0}');
+      if (session && session.expires > new Date().valueOf()) {
+        FB.Auth.setSession(session, 'connected');
+      }
+      console.log('PhoneGap Facebook Connect plugin initialized successfully.');
+    }, (fail?fail:null), 'com.phonegap.facebook.Connect', 'init', [apiKey]);
+  },
+  login: function(params, cb, fail) {
+    params = params || { perms: '' };
+    PhoneGap.exec(function(e) { // login
+        localStorage.setItem('pg_fb_session', JSON.stringify(e.session));
+        FB.Auth.setSession(e.session, 'connected');
+        if (cb) cb(e);
+    }, (fail?fail:null), 'com.phonegap.facebook.Connect', 'login', params.perms.split(',') );
+  },
+  logout: function(cb, fail) {
+    PhoneGap.exec(function(e) {
+      FB.Auth.setSession(null, 'notConnected');
+      if (cb) cb(e);
+    }, (fail?fail:null), 'com.phonegap.facebook.Connect', 'logout', []);
+  },
+  getLoginStatus: function(cb, fail) {
+    PhoneGap.exec(function(e) {
+      if (cb) cb(e);
+    }, (fail?fail:null), 'com.phonegap.facebook.Connect', 'getLoginStatus', []);
+  }
 };
