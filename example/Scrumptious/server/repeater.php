a0eb50fff92c1fe4a8e212a6498921419e97eade
Steven Gill <stevengill97@gmail.com>
2013-04-02 16:52:21 -0700

    updated repeater.js

--- example/Scrumptious/server/repeater.php
+++ example/Scrumptious/server/repeater.php
@@ -17,7 +17,6 @@ function curPageURL() {
       <meta property="og:type" content="<?php echo strip_tags($_REQUEST['og:type']);?>">
       <meta property="og:title" content="<?php echo strip_tags($_REQUEST['og:title']);?>">
       <meta property="og:image" content="<?php echo strip_tags($_REQUEST['og:image']);?>">
-      <meta property="og:description" content="<?php echo strip_tags($_REQUEST['og:description']);?>">
       <title>Product Name</title>
   </head>
     <body>
