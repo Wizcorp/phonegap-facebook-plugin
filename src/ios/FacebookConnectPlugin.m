df8a7816989c8883e254445e31ce65214403a608
Jamie Sutherland <jamie@newbridgegreen.com>
2013-12-03 16:36:01 +0000

    Fixes issue #220 on upstream phonegap/phonegap-facebook-plugin

--- src/ios/FacebookConnectPlugin.m
+++ src/ios/FacebookConnectPlugin.m
@@ -417,14 +417,35 @@
  * A method for parsing URL parameters.
  */
 - (NSDictionary*)parseURLParams:(NSString *)query {
+    NSString *regexStr = @"^(.+)\\[(.*)\\]$";
+    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:regexStr options:0 error:nil];
+
     NSArray *pairs = [query componentsSeparatedByString:@"&"];
     NSMutableDictionary *params = [[NSMutableDictionary alloc] init];
     [pairs enumerateObjectsUsingBlock:
      ^(NSString *pair, NSUInteger idx, BOOL *stop) {
          NSArray *kv = [pair componentsSeparatedByString:@"="];
-         NSString *val = [kv[1]
-                          stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
-         params[kv[0]] = val;
+         NSString *key = [kv[0] stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+         NSString *val = [kv[1] stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+
+         NSArray *matches = [regex matchesInString:key options:0 range:NSMakeRange(0, [key length])];
+         if ([matches count] > 0) {
+             for (NSTextCheckingResult *match in matches) {
+
+                 NSString *newKey = [key substringWithRange:[match rangeAtIndex:1]];
+
+                 if ([[params allKeys] containsObject:newKey]) {
+                     NSMutableArray *obj = [params objectForKey:newKey];
+                     [obj addObject: val];
+                     [params setObject: obj forKey: newKey];
+                 } else {
+                     NSMutableArray *obj = [NSMutableArray arrayWithObject:val];
+                     [params setObject: obj forKey: newKey];
+                 }
+             }
+         } else {
+             params[key] = val;
+         }
     }];
     return params;
 }
