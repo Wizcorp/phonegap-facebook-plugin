cc9044f6179c265ccdce7ae46a66cb9558a454bb
keab42 <keab42@gmail.com>
2014-04-02 15:55:57 +0100

    Update facebookConnectPlugin.js
    Fixing indenting and using copy of correct file to do so...

--- www/phonegap/plugin/facebookConnectPlugin/facebookConnectPlugin.js
+++ www/phonegap/plugin/facebookConnectPlugin/facebookConnectPlugin.js
@@ -1,39 +1,36 @@
-cordova.define("com.phonegap.plugins.facebookconnect.FacebookConnectPlugin", function (require, exports, module) {
-    /*
-     * @author Ally Ogilvie
-     * @copyright Wizcorp Inc. [ Incorporated Wizards ] 2014
-     * @file - facebookConnectPlugin.js
-     * @about - JavaScript interface for PhoneGap bridge to Facebook Connect SDK
-     *
-     *
-     */
+/*
+ * @author Ally Ogilvie
+ * @copyright Wizcorp Inc. [ Incorporated Wizards ] 2014
+ * @file - facebookConnectPlugin.js
+ * @about - JavaScript interface for PhoneGap bridge to Facebook Connect SDK
+ *
+ *
+ */
 
-    var exec = require("cordova/exec");
+var exec = require("cordova/exec");
 
-    var facebookConnectPlugin = {
+var facebookConnectPlugin = {
 
-        getLoginStatus : function (s, f) {
-            cordova.exec(s, f, "FacebookConnectPlugin", "getLoginStatus", []);
-        },
+    getLoginStatus : function (s, f) {
+        cordova.exec(s, f, "FacebookConnectPlugin", "getLoginStatus", []);
+    },
 
-        showDialog : function (options, s, f) {
-            cordova.exec(s, f, "FacebookConnectPlugin", "showDialog", [options]);
-        },
+    showDialog : function (options, s, f) {
+        cordova.exec(s, f, "FacebookConnectPlugin", "showDialog", [options]);
+    },
 
-        login : function (permissions, s, f) {
-            cordova.exec(s, f, "FacebookConnectPlugin", "login", permissions);
-        },
+    login : function (permissions, s, f) {
+        cordova.exec(s, f, "FacebookConnectPlugin", "login", permissions);
+    },
 
-        logout : function (s, f) {
-            cordova.exec(s, f, "FacebookConnectPlugin", "logout", []);
-        },
+    logout : function (s, f) {
+        cordova.exec(s, f, "FacebookConnectPlugin", "logout", []);
+    },
 
-        api : function (graphPath, permissions, s, f) {
-            cordova.exec(s, f, "FacebookConnectPlugin", "graphApi", [graphPath, permissions]);
-        }
+    api : function (graphPath, permissions, s, f) {
+        cordova.exec(s, f, "FacebookConnectPlugin", "graphApi", [graphPath, permissions]);
+    }
 
-    };
+};
 
-    module.exports = facebookConnectPlugin;
-
-});
+module.exports = facebookConnectPlugin;
