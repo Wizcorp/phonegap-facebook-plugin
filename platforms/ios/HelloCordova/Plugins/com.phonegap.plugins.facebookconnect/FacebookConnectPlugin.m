a89ce0e0d7c05dd95d2ee02a667bf160d8fd500b
Jake Craige <jake@poeticsystems.com>
2014-06-09 09:15:46 -0500

    check loginCallbackId exists before pluginResult call
    We make sure that loginCallbackId exists before calling it. When it's not set, it triggers the exception referenced in issue #507

--- platforms/ios/HelloCordova/Plugins/com.phonegap.plugins.facebookconnect/FacebookConnectPlugin.m
+++ platforms/ios/HelloCordova/Plugins/com.phonegap.plugins.facebookconnect/FacebookConnectPlugin.m
@@ -71,21 +71,25 @@
                      ^(FBRequestConnection *connection, id <FBGraphUser>user, NSError *error) {
                          if (!error) {
                              self.userid = user.id;
+                             
                              // Send the plugin result. Wait for a successful fetch of user info.
-                             CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
+                             if(self.loginCallbackId) {
+                                CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
                                                                            messageAsDictionary:[self responseObject]];
-                             [self.commandDelegate sendPluginResult:pluginResult callbackId:self.loginCallbackId];
+                                [self.commandDelegate sendPluginResult:pluginResult callbackId:self.loginCallbackId];
+                             }
                          } else {
                              self.userid = @"";
-                             
                          }
                      }];
                 }else {
                     // Don't get user's info but trigger success callback
                     // Send the plugin result. Wait for a successful fetch of user info.
-                    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK 
+                    if(self.loginCallbackId) {
+                        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK 
                                                                 messageAsDictionary:[self responseObject]];
-                    [self.commandDelegate sendPluginResult:pluginResult callbackId:self.loginCallbackId];
+                        [self.commandDelegate sendPluginResult:pluginResult callbackId:self.loginCallbackId];
+                    }
                 }
             }
             break;
@@ -127,7 +131,7 @@
             alertMessage = @"Error. Please try again later.";
         }
         
-        if (alertMessage) {
+        if (alertMessage && self.loginCallbackId) {
             CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
                                                               messageAsString:alertMessage];
             [self.commandDelegate sendPluginResult:pluginResult callbackId:self.loginCallbackId];
