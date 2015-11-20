0c7685047400aa61604185e6113ca1cccb2b1a53
Jeduan Cornejo <jeduan@gmail.com>
2015-11-19 21:49:31 -0600

    iOS SDK v4.7

--- src/ios/Bolts.framework/Headers/Bolts.h
+++ src/ios/Bolts.framework/Headers/Bolts.h
@@ -17,9 +17,9 @@
 #import <Bolts/BFTask.h>
 #import <Bolts/BFTaskCompletionSource.h>
 
-#if __has_include(<Bolts/BFAppLink.h>) && TARGET_OS_IPHONE
-#import <Bolts/BFAppLinkNavigation.h>
+#if __has_include(<Bolts/BFAppLink.h>) && TARGET_OS_IPHONE && !TARGET_OS_WATCH && !TARGET_OS_TV
 #import <Bolts/BFAppLink.h>
+#import <Bolts/BFAppLinkNavigation.h>
 #import <Bolts/BFAppLinkResolving.h>
 #import <Bolts/BFAppLinkReturnToRefererController.h>
 #import <Bolts/BFAppLinkReturnToRefererView.h>
