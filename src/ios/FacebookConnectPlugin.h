f05ce3fdccd44023950e50fbd8c306b6a5349575
Robert Arnesson <robert@robertarnesson.com>
2016-04-29 16:46:28 -0400

    added support for getDeferredApplink

--- src/ios/FacebookConnectPlugin.h
+++ src/ios/FacebookConnectPlugin.h
@@ -25,5 +25,6 @@
 - (void)graphApi:(CDVInvokedUrlCommand *)command;
 - (void)showDialog:(CDVInvokedUrlCommand *)command;
 - (void)appInvite:(CDVInvokedUrlCommand *) command;
+- (void)getDeferredApplink:(CDVInvokedUrlCommand *) command;
 - (void)activateApp:(CDVInvokedUrlCommand *)command;
 @end
