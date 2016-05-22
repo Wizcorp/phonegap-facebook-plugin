da42bd775a3d4b6cda5d80abc6fb216ebd4046f2
ben <ben@standingtreesoftware.com>
2016-05-21 18:13:32 -0600

    #223 fix for signing in as different user
    Sign the previous user out before trying to sign-in

--- src/android/ConnectPlugin.java
+++ src/android/ConnectPlugin.java
@@ -16,6 +16,7 @@ import com.facebook.FacebookSdk;
 import com.facebook.FacebookServiceException;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
+import com.facebook.FacebookAuthorizationException
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
@@ -129,6 +130,12 @@ public class ConnectPlugin extends CordovaPlugin {
             public void onError(FacebookException e) {
                 Log.e("Activity", String.format("Error: %s", e.toString()));
                 handleError(e, loginContext);
+                // Sign-out current instance in case token is still valid for previous user
+                if (e instanceof FacebookAuthorizationException) {
+                    if (AccessToken.getCurrentAccessToken() != null) {
+                        LoginManager.getInstance().logOut();
+                    }
+                }
             }
         });
 
@@ -330,7 +337,7 @@ public class ConnectPlugin extends CordovaPlugin {
                     AppEventsLogger.activateApp(cordova.getActivity());
                 }
             });
-            
+
             return true;
         }
         return false;
