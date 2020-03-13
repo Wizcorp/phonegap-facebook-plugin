a5edc2049a403ada4c5b7a2c9fbb78c8cde3fe51
Peter Maatman <peter.maatman@rentman.nl>
2020-03-13 13:26:52 +0100

    Fix hasAccessToken check (#853)
    When an access token is expired this plugin would not
    fetch a new one. This is invalid behaviour and resulted
    in failed login attempts because the token was expired,
    but still passed to the javascript code as a valid token.
    
    This patch adds a check if the token is expired as well.

--- src/android/ConnectPlugin.java
+++ src/android/ConnectPlugin.java
@@ -746,7 +746,12 @@ public class ConnectPlugin extends CordovaPlugin {
 
     // Simple active session check
     private boolean hasAccessToken() {
-        return AccessToken.getCurrentAccessToken() != null;
+        AccessToken token = AccessToken.getCurrentAccessToken();
+
+		if (token == null)
+			return false;
+
+		return !token.isExpired();
     }
 
     private void handleError(FacebookException exception, CallbackContext context) {
